408a14319201acf2969d3999e81d0e3a6238b4bc
chirrashruthigoud <chirrashruthigoud4@gmail.com>
2023-05-08 22:11:02 +0530

    Add : Duplicate Element

---
+++ Day4B_ObjectorientedPractice/Day4B_ObjectorientedPractice/DuplicateElement.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day4B_ObjectorientedPractice
+{
+    internal class DuplicateElement
+    {
+        public void elements()
+        {
+            int[] arr = { 1, 2, 3, 4, 5, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            int count = 0;
+
+            for (int i = 0; i < arr.Length - 1; i++)
+            {
+                for (int j = i + 1; j < arr.Length; j++)
+                {
+                    if (arr[i] == arr[j])
+                    {
+                        count++;
+                        break;
+                    }
+                }
+            }
+
+            Console.WriteLine("The total number of duplicate elements in the array is: " + count);
+        }
+
+    }
+
+}
---
+++ Day4B_ObjectorientedPractice/Day4B_ObjectorientedPractice/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day4B_ObjectorientedPractice
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+             DuplicateElement num=new DuplicateElement();
+             num.elements();
+            Console.ReadKey();
+        }
+    }
+}
